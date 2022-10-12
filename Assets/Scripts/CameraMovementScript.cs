0ec9898d8c89d74721396e33905e746b798b8e3d
NilsCarron <nilscarron@yahoo.fr>
2022-10-12 16:11:57 +0200

    Adding search and movement scripts

---
+++ Assets/Scripts/CameraMovementScript.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementScript : MonoBehaviour
+{
+
+    float speed = 0.06f;
+    float zoomSpeed = 10.0f;
+    float roateSpeed;
+    float maxHeight = 40;
+    float minHeight = 4f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 0.06f;
+            zoomSpeed = 20.0f;
+        }
+        else
+        {
+            speed = 0.035f;
+            zoomSpeed = 10.0f;
+
+        }
+
+
+        float hsp = speed * Input.GetAxis("Horizontal");
+        float vsp = speed * Input.GetAxis("Vertical");
+        float scrollSp = -zoomSpeed * Input.GetAxis("Mouse ScrollWheel");
+
+
+        if ((transform.position.y >= maxHeight) && scrollSp < 0)
+        {
+            scrollSp = 0;
+        }
+        else if((transform.position.y <= maxHeight) && scrollSp < 0)
+        {
+            scrollSp = 0;
+        }
+        if((transform.position.y + scrollSp) > maxHeight)
+        {
+            scrollSp = maxHeight - transform.position.y;
+        }
+        else if((transform.position.y + scrollSp) < minHeight)
+        {
+            scrollSp = minHeight - transform.position.y;
+        }
+
+
+        Vector3 verticalMove = new Vector3(0, scrollSp, 0);
+        Vector3 lateralMove = hsp * transform.right;
+        Vector3 forwardMove = transform.forward;
+        forwardMove.y = 0;
+        forwardMove.Normalize();
+        forwardMove *= vsp;
+        Vector3 move = verticalMove + lateralMove + forwardMove;
+
+        transform.position += move;
+    }
+}
---
+++ Assets/Scripts/base_behavior.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class base_behavior : MonoBehaviour
+{
+
+    public int team;
+
+    //links to the different behavior components
+    public seek_script seek;
+
+    //gps is our general pathfinding script
+    //public general_pathfinding gps;
+
+    //intelligent movement scripts
+    public Agent agentScript;
+
+    public Seek seekScript;
+    public Flee fleeScript;
+
+    public float maxSpeed;
+
+    public GameObject target;
+    public UnitFSM state;
+
+    public enum UnitFSM //states
+    {
+        Attack,
+        Seek,
+        Idle
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agentScript = gameObject.AddComponent<Agent>(); //add agent
+        agentScript.maxSpeed = maxSpeed;
+
+        changeState(UnitFSM.Idle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if(state == UnitFSM.Idle)
+            {
+                changeState(UnitFSM.Seek);
+            }
+            else
+            {
+                changeState(UnitFSM.Idle);
+            }
+        }
+    }
+
+    public void changeState(UnitFSM new_state)
+    {
+
+        state = new_state;
+
+        switch (new_state)
+        {
+            case UnitFSM.Idle:
+
+                DestroyImmediate(seek);
+
+                break;
+
+            case UnitFSM.Seek:
+
+                if (gameObject.GetComponent<seek_script>() == null)
+                {
+                    seek = gameObject.AddComponent<seek_script>();
+                }
+
+                break;
+
+            case UnitFSM.Attack:
+
+
+                DestroyImmediate(seek);
+
+                break;
+
+
+
+        }
+    }
+}
